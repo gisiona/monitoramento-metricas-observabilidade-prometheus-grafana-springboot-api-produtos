61fc59d41c71f6af8dc27e0cd60ce6aceda58d0f
gisiona <gil-real@hotmail.com>
2022-11-02 23:37:18 -0300

    add metrics duration e ajuste no dashboard json

--- produto/src/main/java/com/gisiona/observabilidade/produtos/api/ProdutoController.java
+++ produto/src/main/java/com/gisiona/observabilidade/produtos/api/ProdutoController.java
@@ -4,12 +4,15 @@ import com.gisiona.observabilidade.produtos.client.avaliacoes.AvaliacaoClient;
 import com.gisiona.observabilidade.produtos.client.avaliacoes.AvaliacaoModel;
 import com.gisiona.observabilidade.produtos.domain.Produto;
 import com.gisiona.observabilidade.produtos.domain.ProdutoRepository;
+import io.prometheus.client.CollectorRegistry;
+import io.prometheus.client.Summary;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
 
 @RestController
@@ -19,13 +22,28 @@ public class ProdutoController {
 	private final ProdutoRepository produtos;
 	private final AvaliacaoClient avaliacoes;
 
-	public ProdutoController(ProdutoRepository produtos, AvaliacaoClient avaliacoes) {
+	private final Summary requestDuration;
+	private final CollectorRegistry collectorRegistry;
+
+	public ProdutoController(ProdutoRepository produtos, AvaliacaoClient avaliacoes, CollectorRegistry collectorRegistry) {
 		this.produtos = produtos;
 		this.avaliacoes = avaliacoes;
+		this.collectorRegistry = collectorRegistry;
+		requestDuration = Summary.build()
+				.name("http_request_duration_summary_gisiona")
+				.help("Time for http request")
+				.quantile(0.95, 0.01)
+				.register(collectorRegistry);
 	}
 
 	@GetMapping
 	public List<ProdutoModel> buscarTodos() {
+		Summary.Timer timer = requestDuration.startTimer();
+
+		Long sleepDuration = Double.valueOf(Math.floor(Math.random() * 10 * 1000)).longValue();
+
+		timer.observeDuration();
+
 		return produtos.getAll()
 				.stream()
 				.map(this::converterProdutoParaModelo)
